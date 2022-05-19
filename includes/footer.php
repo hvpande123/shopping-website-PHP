3970374ad15f0eb799927afdb230235bcba26fcd
profcess <profcess>
2022-05-19 21:48:27 +0530

    Added my project

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy; Shopify | All Rights Reserved    by Harshvardhan | Contact Us: +91 90000 00000</b></span></div>
+</footer>
+    
